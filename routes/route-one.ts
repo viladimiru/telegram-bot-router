1835cb46224fc6a53e3052ff59c423115f20b27e
viladimiru <bobahdro@gmail.com>
2024-12-21 15:54:49 +0300

    format code

--- routes/route-one.ts
+++ routes/route-one.ts
@@ -1,5 +1,5 @@
 export function routeOne(props: { one: number }): never {
-	console.log(props);
-	// @ts-expect-error this is just a plug
-	return ['text', {} as never];
+  console.log(props);
+  // @ts-expect-error this is just a plug
+  return ['text', {} as never];
 }
--- storage.ts
+++ storage.ts
@@ -1,22 +1,22 @@
 export interface RouteData {
-	path: string;
-	props: Record<string, unknown>;
+  path: string;
+  props: Record<string, unknown>;
 }
 
 interface Storage {
-	saveSession: (chatId: number, routeData: RouteData) => void;
-	getSession: (chatId: number) => RouteData | undefined;
+  saveSession: (chatId: number, routeData: RouteData) => void;
+  getSession: (chatId: number) => RouteData | undefined;
 }
 
 export function createStorage(): Storage {
-	const sessionStorage = new Map<number, RouteData>()
-	return {
-		saveSession: (chatId, routeData) => {
-			sessionStorage.set(chatId, routeData);
-		},
-		getSession: (chatId) => {
-			const session = sessionStorage.get(chatId);
-			return session
-		},
-	}
+  const sessionStorage = new Map<number, RouteData>();
+  return {
+    saveSession: (chatId, routeData) => {
+      sessionStorage.set(chatId, routeData);
+    },
+    getSession: (chatId) => {
+      const session = sessionStorage.get(chatId);
+      return session;
+    },
+  };
 }
