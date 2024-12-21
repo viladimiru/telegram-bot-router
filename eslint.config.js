7943f0f994a22a61d86281da0ff63b45eabf6345
viladimiru <bobahdro@gmail.com>
2024-12-21 15:54:50 +0300

    show prettier errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,19 @@
 // @ts-check
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  prettier,
+  {
+    extends: [eslintConfigPrettier],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
 );
