a1d9cbe93946f4f11b326cdb788251755101bfcb
viladimiru <bobahdro@gmail.com>
2024-12-21 15:54:49 +0300

    resolve todos

--- create-route.ts
+++ create-route.ts
@@ -1,6 +1,6 @@
 import { SendMessageOptions } from 'node-telegram-bot-api';
 
-export type SendMessage = (chatId: number, message: string, options: SendMessageOptions) => void;
+export type SendMessage = (message: string, options: SendMessageOptions) => void;
 export type Props = Record<string, unknown>;
 export type RenderReturnType = [
 	string,
@@ -9,8 +9,7 @@ export type RenderReturnType = [
 
 export interface Route {
 	render(props: Props): RenderReturnType;
-	// TODO: get rid of chatId
-	onAnswer(props: Props, chatId: number, sendMessage: SendMessage): void;
+	onAnswer(props: Props, sendMessage: SendMessage): void;
 }
 
 export function createRoute<R extends Route>(route: R): R {
